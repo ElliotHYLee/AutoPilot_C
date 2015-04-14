4b06f7ec501baafc0c02b44475a9503b68242453
ElliotHYLee <hylee101001@gmail.com>
2015-04-14 00:38:16 -0400

    dd

---
+++ Motors.c
@@ -0,0 +1,11 @@
+/*
+  Blank Simple Project.c
+  http://learn.parallax.com/propeller-c-tutorials 
+*/
+#include "simpletools.h"                      // Include simple tools
+
+int main()                                    // Main function
+{
+
+
+}
